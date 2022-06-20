f9998e04abee50765b4d0e78f4ce7717e44569de
Martin Chládek <mates.chladek@gmail.com>
2022-06-20 09:40:55 +0200

    Add styles to compilation

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
 // COMPONENTS
 export * from './react/components';
+
+// Leaflet styles
+import "leaflet/dist/leaflet.css";
+import 'leaflet.markercluster/dist/MarkerCluster.css';
+import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
+
+// Geovisto styles
+import "geovisto/dist/index.css";
+import "geovisto-sidebar/dist/index.css";
+import "geovisto-filters/dist/index.css";
+import 'geovisto-layer-choropleth/dist/index.css';
+import 'geovisto-layer-marker/dist/index.css';
+import 'geovisto-layer-connection/dist/index.css';
